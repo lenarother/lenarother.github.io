b65fb09f3d95f9dc856882d87ef6fdb13f651ee8
Magdalena Rother <magdalena.rother@thermondo.de>
2023-03-20 00:55:13 +0100

    Add keys file example

---
+++ idtoken/secrets.example.py
@@ -0,0 +1,18 @@
+PUBLIC_KEY = b"""-----BEGIN PUBLIC KEY-----
+foobar123
+"""
+
+PRIVATE_KEY = b"""-----BEGIN RSA PRIVATE KEY-----
+foobar123
+-----END RSA PRIVATE KEY-----
+"""
+
+PUBLIC_KEY_EC = b"""-----BEGIN PUBLIC KEY-----
+foobar123
+-----END PUBLIC KEY-----
+"""
+
+PRIVATE_KEY_EC = b"""-----BEGIN EC PRIVATE KEY-----
+foobar123
+-----END EC PRIVATE KEY-----
+"""
